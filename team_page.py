e4c6e8f8885699fce2ad92093eeaf6dad2ab404c
Nnamdi Isichei <71382968+isichei-nnamdi@users.noreply.github.com>
2025-07-26 06:49:03 +0100

    Update team_page.py

--- team_page.py
+++ team_page.py
@@ -142,6 +142,7 @@ def show_team_page(go_to):
 
     # --- Convert to DataFrame and show ---
     summary_df = pd.DataFrame(summary_data)
+    st.write(summary_df)
     total_assigned = summary_df["Total Assigned"].sum()
     total_contacted = summary_df["Contacted"].sum()
     total_not_contacted = summary_df["Not Contacted"].sum()

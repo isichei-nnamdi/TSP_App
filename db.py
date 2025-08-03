e309bca1d5296ccec9c7630673c51c4df4d9adea
Nnamdi Isichei <71382968+isichei-nnamdi@users.noreply.github.com>
2025-08-03 11:24:10 +0100

    Update db.py

--- db.py
+++ db.py
@@ -13,10 +13,10 @@ from email.mime.multipart import MIMEMultipart
 
 # --- Supabase PostgreSQL Connection Setup ---
 # Replace with your actual Supabase DB credentials
-DB_HOST = st.secrets[DB_HOST]
-DB_NAME = st.secrets[DB_NAME]
-DB_USER = st.secrets[DB_USER]
-DB_PASSWORD = st.secrets[DB_PASSWORD]
+DB_HOST = st.secretsst["secrets"]["DB_HOST"]
+DB_NAME = st.secrets["secrets"]["DB_NAME"]
+DB_USER = st.secrets["secrets"]["DB_USER"]
+DB_PASSWORD = st.secrets["secrets"]["DB_PASSWORD"]
 DB_PORT = st.secrets.get(DB_PORT, 5432)  # default PostgreSQL port
 
 # DB_PATH = "fta.db"

912c65a7b87ca473332270bdaff76832f3e36f2c
Nnamdi Isichei <71382968+isichei-nnamdi@users.noreply.github.com>
2025-08-28 03:51:00 +0100

    Update fta_tracking.py

--- fta_tracking.py
+++ fta_tracking.py
@@ -144,7 +144,9 @@ def show_feedback_tracking_page(go_to):
     
             st.success("✅ Feedback submitted successfully!")
             st.rerun()
-    
+    with col3:
+         st.write("")
+        
         st.markdown("---")
         st.subheader("📜 Contact History")
     
@@ -155,5 +157,5 @@ def show_feedback_tracking_page(go_to):
             st.dataframe(df_history.sort_values("submitted_at", ascending=False), use_container_width=True)
         else:
             st.info("No feedback history yet.")
-    with col3:
-         st.write("")
+    # with col3:
+    #      st.write("")
